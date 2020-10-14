2a1a82c4719879f5c7b1948416a29f8f433d1a08
Tera0592 <69949838+Tera0592@users.noreply.github.com>
2020-10-14 11:00:01 +0900

    recipesテーブル作成

--- db/migrate/20201014011009_create_recipes.rb
+++ db/migrate/20201014011009_create_recipes.rb
@@ -1,8 +1,13 @@
 class CreateRecipes < ActiveRecord::Migration[6.0]
   def change
     create_table :recipes do |t|
-      t.string :index
-
+      
+      t.integer :user_id,       foreign_key: true
+      t.string  :name,          null: false
+      t.integer :genre_id,      null: false
+      t.text    :food,          null: false
+      t.text    :seasoning,     null: false
+      t.text    :procedure,     null: false
       t.timestamps
     end
   end
