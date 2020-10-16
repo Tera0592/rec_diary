d78505f259b011fc022717feec84181511220495
Tera0592 <69949838+Tera0592@users.noreply.github.com>
2020-10-16 14:05:19 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   validates :nickname,        presence:                                        {                          message: "は20文字以内で入力してください。"} 
   validates :email,           presence: true, uniqueness: true,        format: { with: /[@]/,             message: "は@を含めて入力してください。"}
   validates :password,        presence: true, length: { minimum: 6 },  format: { with: /\A[a-z0-9]+\z/i,  message: "は6文字以上の半角英数字で入力してください。" }
+
+  has_many  :recipes
 end
