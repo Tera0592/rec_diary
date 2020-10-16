c609940efb0cc334503a9b8a4544466ee730e834
Tera0592 <69949838+Tera0592@users.noreply.github.com>
2020-10-16 14:03:35 +0900

    userバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
+  validates :nickname,        presence:                                        {                          message: "は20文字以内で入力してください。"} 
+  validates :email,           presence: true, uniqueness: true,        format: { with: /[@]/,             message: "は@を含めて入力してください。"}
+  validates :password,        presence: true, length: { minimum: 6 },  format: { with: /\A[a-z0-9]+\z/i,  message: "は6文字以上の半角英数字で入力してください。" }
 end
