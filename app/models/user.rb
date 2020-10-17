af622c163cb9863e07bf48d9927f8dcf4734b78e
Tera0592 <69949838+Tera0592@users.noreply.github.com>
2020-10-17 10:38:04 +0900

    userエラーメッセージの修正(2)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable
-  validates :nickname,        presence:                                        {                          message: "は20文字以内で入力してください。"} 
-  validates :email,           presence: true, uniqueness: true,        format: { with: /[@]/,             message: "は@を含めて入力してください。"}
-  validates :password,        presence: true, length: { minimum: 6 },  format: { with: /\A[a-z0-9]+\z/i,  message: "は6文字以上の半角英数字で入力してください。" }
+  validates :nickname,              presence:                                        {                          message: "は20文字以内で入力してください。"} 
+  validates :email,                 presence: true, uniqueness: true,        format: { with: /[@]/,             message: "は@を含めて入力してください。"}
+  validates :password,              presence: true, length: { minimum: 6 },  format: { with: /\A[a-z0-9]+\z/i,  message: "は6文字以上の半角英数字で入力してください。" }
+  validates :password_confirmation, presence:                                        {                          message: "を入力してください。"} 
 
   has_many  :recipes
 end
