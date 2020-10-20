a4a4c6d416d241a9314ba907b59a88f6e774929d
Tera0592 <69949838+Tera0592@users.noreply.github.com>
2020-10-20 13:24:08 +0900

    recipesテーブルのバリデーション

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,13 @@
 class Recipe < ApplicationRecord
+  # バリデーション
+  
+  # 空のカラムは保存ができない
+  validates :name, :image, :genre, :food, :seasoning, :procedure, presence: true
+  # ジャンルの選択が「---」の時は保存できないようにする
+  validates :genre_id, numericality: { other_than: 1 }
+
+  # アソシエーション
+  
   belongs_to :user
   has_one_attached :image
 
