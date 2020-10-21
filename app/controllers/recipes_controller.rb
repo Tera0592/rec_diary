444b9cfdca02ff388264f28df445d47205686c4e
Tera0592 <69949838+Tera0592@users.noreply.github.com>
2020-10-21 14:33:06 +0900

    recipesコントローラーの設定(create)

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,4 +5,22 @@ class RecipesController < ApplicationController
   def new
     @recipe = Recipe.new
   end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:image, :name, :genre_id, :food, :seasoning, :procedure).merge(user_id: current_user.id)
+  end
+
 end
+
+
