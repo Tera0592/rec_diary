31edc1c4fab4065a51a61c86253d87ea8c672792
Tera0592 <69949838+Tera0592@users.noreply.github.com>
2020-10-26 14:16:40 +0900

    menuの設定

---
+++ app/javascript/menu.js
@@ -0,0 +1,13 @@
+$(function(){
+  $("#user").on("click",function(){
+    if ($('#menu').css('display') == 'none') {
+      $("#menu").css("display", "block")
+      return false;
+      // 表示されている場合の処理
+  } else {
+      $("#menu").css("display", "none")
+      return false;  
+      // 非表示の場合の処理
+  }
+  });
+});
