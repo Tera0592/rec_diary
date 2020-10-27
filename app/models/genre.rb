17ed3eba02806369364e393e32deb8537cabbb26
Tera0592 <69949838+Tera0592@users.noreply.github.com>
2020-10-27 10:49:09 +0900

    genreカラムの修正(1)

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,5 @@
 class Genre < ActiveHash::Base
   self.data = [
-    {id: 1, name: '---'}, {id: 2, name: '和食'}, {id: 3, name: '洋食'}, {id: 4, name: '中華料理'}
+    {id: 1, name: '---'}, {id: 2, name: '和食'}, {id: 3, name: '洋食'}, {id: 4, name: '中華料理'}, {id: 5, name: 'デザート'}
   ]
 end
