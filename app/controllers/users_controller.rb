6a49f4cdd62198dec98dafe9d6455251e0166002
Tera0592 <69949838+Tera0592@users.noreply.github.com>
2020-10-29 10:23:45 +0900

    usersコントローラーの設定(update)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,18 @@ class UsersController < ApplicationController
     
   end
 
+  def update
+    if current_user.update(user_params)
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:nickname, :email)
+  end
+
 end
