e8f72cb54f86a130cad5cebfa78054f25ec242aa
Tera0592 <69949838+Tera0592@users.noreply.github.com>
2020-11-02 15:45:32 +0900

    menu.jsの修正(2)

--- app/javascript/menu.js
+++ app/javascript/menu.js
@@ -1,13 +1,5 @@
-$(document).ready(function(){
-  $("#user").on("click",function(){
-    if ($('#menu').css('display') == 'none') {
-      $("#menu").css("display", "block")
-      return false;
-      // 表示されている場合の処理
-  } else {
-      $("#menu").css("display", "none")
-      return false;  
-      // 非表示の場合の処理
-  }
+$(function(){
+  $(".user-nickname").hover(function(){
+    $(this).children(".my-menu").stop().slideToggle();
   });
 });
