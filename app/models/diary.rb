2d4ee39295c81923da77a26313b732718230602f
Tera0592 <69949838+Tera0592@users.noreply.github.com>
2020-11-07 11:56:01 +0900

    diariesテーブルのバリデーション

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,7 +1,17 @@
 class Diary < ApplicationRecord
 
+  # バリデーション
+
+  # 空のカラムは保存ができない
+  
+  validates :time,                         presence:                { message: "を選択してください。" }
+  validates :image,                        presence:                { message: "のファイルを選択してください。" }
+  validates :title, :impression,           presence:                { message: "を入力してください。" }
+
+  
   # アソシエーション
   
   belongs_to :user
-  
+  has_one_attached :image
+
 end
