4fd3eb402fffd29449a935cc23acad2e99d3e39f
Tera0592 <69949838+Tera0592@users.noreply.github.com>
2020-11-08 10:51:33 +0900

    diariesコントローラーの設定(create)

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -7,5 +7,20 @@ class DiariesController < ApplicationController
   def new
     @diary = Diary.new
   end
+
+  def create
+    @diary = Diary.new(diary_params)
+    if @diary.save
+      redirect_to diaries_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def diary_params
+    params.require(:diary).permit(:image, :time, :title, :impression).merge(user_id: current_user.id)
+  end
   
 end
