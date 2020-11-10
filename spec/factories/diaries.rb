704952c2f09e9e63e90329985bec44fe9125ba2d
Tera0592 <69949838+Tera0592@users.noreply.github.com>
2020-11-10 16:50:07 +0900

    diaryモデルテストの準備

--- spec/factories/diaries.rb
+++ spec/factories/diaries.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :diary do
-    
+    time        { Faker::Date.forward }
+    title       { Faker::Lorem.word }
+    impression  { Faker::Lorem.sentences }
+
+    after(:build) do |diary|
+      diary.image.attach(io: File.open('spec/fixtures/diary/diary_test_image.jpg'), filename: 'diary_test_image.jpg', content_type: 'image/jpg')
+    end
+
+    association :user
+
   end
 end
