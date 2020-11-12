6965e49230ec2c7726214d0b354c80b3d6acc0d3
Tera0592 <69949838+Tera0592@users.noreply.github.com>
2020-11-12 14:35:21 +0900

    diary結合テスト(日記詳細)

--- spec/system/diaries_spec.rb
+++ spec/system/diaries_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe "日記投稿", type: :system do
       }.to change { Diary.count }.by(1)
       # 日記一覧ページに遷移する
       expect(current_path).to eq diaries_path
-      # トップページには先ほど投稿した日記が存在する
+      # 日記一覧ページには先ほど投稿した日記が存在する
       expect(page).to have_selector "img[src$='diary_test_image.jpg']"
       
     end
@@ -54,6 +54,69 @@ RSpec.describe "日記投稿", type: :system do
 
   end
 
+end
+
+RSpec.describe "日記詳細", type: :system do
+  before do
+    @diary1 = FactoryBot.create(:diary)
+    @another = FactoryBot.create(:user)
+  end
+  
+  context "日記詳細ページが閲覧できるとき" do
+
+    it 'ログインしたユーザーが日記詳細ページを閲覧できる' do
+      
+      # 新規登録済みのユーザーでログインする
+      log_in(@diary1.user)
+      # ニックネームの部分に、マウスを持ってくと"日記一覧"へ遷移するボタンがある
+      expect(
+        find(".user-nickname").hover
+      ).to have_content ('日記一覧')
+      # 日記一覧ページに移動する
+      visit diaries_path
+      # 投稿されている日記の"▼"部分に、マウスを持ってくと、"詳細"へ遷移するボタンがある
+      expect(
+        find(".preview").hover
+      ).to have_content ('詳細')
+      # 日記詳細ページへ遷移する
+      visit diary_path(@diary1)
+      # 日記詳細ページに日記の内容が含まれる
+      expect(page).to have_selector "img[src$='diary_test_image.jpg']"
+      expect(page).to have_content(@diary1.title)
+      expect(page).to have_content(@diary1.impression)
+      
+    end
+
+  end
+
+  context "日記詳細ページが閲覧できないとき" do
+
+    it 'ログインしていないとユーザーは日記詳細ページを閲覧できない' do
+      
+      # トップページに遷移する
+      visit root_path
+      # 日記一覧ページへのリンクがない
+      expect(page). to have_no_content('日記一覧')
+      
+    end
+
+    it 'ログインしても、投稿されている日記のユーザー以外は日記詳細ページを閲覧できない' do
+      
+      # すでに投稿されている日記のユーザー以外のユーザーでログインする
+      log_in(@another)
+      # ニックネームの部分に、マウスを持ってくと"日記一覧"へ遷移するボタンがある
+      expect(
+        find(".user-nickname").hover
+      ).to have_content ('日記一覧')
+      # 日記一覧ページに移動する
+      visit diaries_path
+      # 日記一覧ページに、ログインユーザー以外の日記は存在しない
+      expect(page).to have_no_selector "img[src$='diary_test_image.jpg']"
+      
+    end
+
+  end
+  
 end
 
 
